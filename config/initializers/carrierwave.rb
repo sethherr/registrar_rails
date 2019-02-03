cc04140397482bc066c3f889b9c0b0543f4a1808
sethherr <seth.william.herr@gmail.com>
2019-02-02 19:55:05 -0800

    add aws and sparkpost

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -34,19 +34,20 @@ end
 CarrierWave.configure do |config|
   config.cache_dir = "#{Rails.root}/tmp/uploads"
 
-  # if Rails.env.production?
-  #   config.fog_provider "fog/aws"
-  #   config.fog_credentials = {
-  #     provider: "AWS",
-  #     aws_access_key_id: Rails.application.credentials.dig(:aws, :access_id),
-  #     aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_key),
-  #     region: "us-west-1"
-  #   }
-  #   config.fog_directory = Rails.application.credentials.dig(:aws, :bucket_name)
-  #   config.fog_attributes = { "Cache-Control" => "max-age=315576000" }
+  if Rails.env.production?
+    config.fog_provider "fog/aws"
+    config.asset_host = "https://files.globalidregistrar.net"
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_id),
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_key),
+      region: "us-west-1"
+    }
+    config.fog_directory = Rails.application.credentials.dig(:aws, :bucket_name)
+    config.fog_attributes = { "Cache-Control" => "max-age=315576000" }
 
-  #   config.storage :fog
-  # else
+    config.storage :fog
+  else
     config.storage :file
-  # end
+  end
 end
