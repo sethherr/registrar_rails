2496217919dafb1ec4eae4a75a0dfe2260c1391b
sethherr <seth.william.herr@gmail.com>
2019-02-04 14:31:19 -0800

    admin registrations controller

---
+++ app/controllers/admin/registrations_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Admin::RegistrationsController < Admin::BaseController
+  include SortableTable
+  before_action :find_registration, except: [:index]
+
+  def index
+    page = params[:page] || 1
+    per_page = params[:per_page] || 100
+    @registrations = Registration.reorder("#{sort_column} #{sort_direction}").page(page).per(per_page)
+  end
+
+  def show; end
+
+  private
+
+  def sortable_columns
+    %w[created_at email name]
+  end
+
+  def find_registration
+    @registration = Registration.where(id: params[:id]).first
+    raise ActiveRecord::RecordNotFound unless @registration.present?
+  end
+end
---
+++ spec/requests/admin/registrations_request_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+base_url = "/admin/registrations"
+
+RSpec.describe base_url, type: :request do
+  let(:subject) { FactoryBot.create(:user) }
+  context "not logged in" do
+    describe "index" do
+      it "redirects" do
+        get base_url
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  context "logged in as user" do
+    include_context :logged_in_as_user
+    describe "index" do
+      it "redirects" do
+        get base_url
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+
+  context "logged in as admin" do
+    include_context :logged_in_as_staff
+    let(:user) { FactoryBot.create(:user, admin_role: admin_role) }
+    let(:admin_role) { "manager" }
+
+    describe "index" do
+      it "renders" do
+        get base_url
+        expect(response.code).to eq "200"
+        expect(response).to render_template("admin/registrations/index")
+      end
+    end
+
+    describe "show" do
+      let(:external_registration) { FactoryBot.create(:external_registration_bike_index) }
+      let!(:registration) { external_registration.registration }
+      it "renders" do
+        get base_url
+        expect(response.code).to eq "200"
+        expect(response).to render_template("admin/registrations/index")
+      end
+    end
+  end
+end
