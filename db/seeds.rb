ef8f653b1c6586d9672de3c243963834114345d5
sethherr <seth.william.herr@gmail.com>
2019-02-14 14:00:44 -0800

    improve it

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@
 initial_tags = ["Appliance", "Art", "ATV/UTV,Snowmobile", "Auto part", "Aviation", "Baby & kid stuff", "Bicycle part", "Bicycles", "Boat part", "Boat", "Book",
                 "Cars/Truck", "CD/DVD/VHS", "Cell Phone", "Clothing/Accessory", "Jewelry" "Collectible", "Computer part", "Computer", "Electronics", "Farm/garden", "Furniture",
                 "Heavy equipment", "Household item", "Material", "Motorcycle part", "Motorcycle/Scooters", "Musical instrument", "Photo/Video", "RV", "Sporting Goods",
-                "Tcket", "Tools", "Toy/Game", "Trailer", "Video game"]
+                "Tcket", "Tools", "Toy/Game", "Trailer", "Video game", "Pet"]
 initial_tags.each { |t| Tag.create(name: t, main_category: true) }
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -7,11 +7,13 @@ RSpec.describe Tag, type: :model do
 
   describe "child and parent tags" do
     let(:tag_parent) { FactoryBot.create(:tag) }
-    let!(:tag_child1) { FactoryBot.create(:tag, parent_id: tag_parent.id) }
-    let!(:tag_child2) { FactoryBot.create(:tag, parent_id: tag_parent.id) }
-    let!(:tag_grandchild) { FactoryBot.create(:tag, parent_id: tag_child2.id) }
-    let!(:tag_great_grandchild) { FactoryBot.build(:tag, parent_id: tag_grandchild.id) }
+    let(:tag_child1) { FactoryBot.create(:tag, parent_id: tag_parent.id) }
+    let(:tag_child2) { FactoryBot.create(:tag, parent_id: tag_parent.id) }
+    let(:tag_grandchild) { FactoryBot.create(:tag, parent_id: tag_child2.id) }
+    let(:tag_great_grandchild) { FactoryBot.build(:tag, parent_id: tag_grandchild.id) }
     it "returns correct association" do
+      expect(Tag.count).to eq 0
+      [tag_child1, tag_child2, tag_grandchild, tag_great_grandchild].each { |t| expect(t).to be_present }
       expect(Tag.count).to eq 4
       expect(tag_child1.parent_tags).to eq([tag_parent])
       expect(tag_parent.child_tags.pluck(:id)).to match_array([tag_child1.id, tag_child2.id, tag_grandchild.id])
