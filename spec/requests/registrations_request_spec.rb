8919fd10c2c36d3ed7f50ca0a60c83c073b0e3f0
sethherr <seth.william.herr@gmail.com>
2019-02-14 13:51:14 -0800

    remove commented out code

--- spec/requests/registrations_request_spec.rb
+++ spec/requests/registrations_request_spec.rb
@@ -88,43 +88,5 @@ RSpec.describe "/registrations", type: :request do
         expect(user.attestations).to eq([attestation])
       end
     end
-
-    # describe "/registrations/edit" do
-    #   it "renders" do
-    #     get "/registrations/#{registration.to_param}/edit"
-    #     expect(response.code).to eq "200"
-    #     expect(response).to render_template("registrations/edit")
-    #   end
-    #   context "not users registration" do
-    #     let(:registration) { FactoryBot.create(:registration_with_owner) }
-    #     it "404s" do
-    #       expect(registration.current_owner).to_not eq user
-    #       expect do
-    #         get "/registrations/#{registration.to_param}/edit"
-    #       end.to raise_error
-    #       expect(response.code).to eq "404"
-    #     end
-    #   end
-    # end
-
-    # describe "update" do
-    #   let(:params) do
-    #     {
-    #       email: "rad_new_email@bike.com",
-    #       admin_role: "manager",
-    #       name: "cooool name"
-    #     }
-    #   end
-    #   it "updates a user" do
-    #     og_email = user.email
-    #     put "/registrations", params: { user: params }
-    #     expect(response).to redirect_to registrations_path
-    #     expect(flash[:success]).to be_present
-    #     user.reload
-    #     expect(user.admin_role).to eq "not"
-    #     expect(user.name).to eq "cooool name"
-    #     expect(user.email).to eq og_email
-    #   end
-    # end
   end
 end
