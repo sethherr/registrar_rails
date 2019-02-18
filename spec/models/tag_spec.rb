56b216b3de0dab32c226c96966a2f314f6f4071b
sethherr <seth.william.herr@gmail.com>
2019-02-18 12:50:49 -0800

    fix spec issue

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -12,9 +12,9 @@ RSpec.describe Tag, type: :model do
     let(:tag_grandchild) { FactoryBot.create(:tag, parent_id: tag_child2.id) }
     let(:tag_great_grandchild) { FactoryBot.build(:tag, parent_id: tag_grandchild.id) }
     it "returns correct association" do
-      expect(Tag.count).to eq 0
-      [tag_child1, tag_child2, tag_grandchild, tag_great_grandchild].each { |t| expect(t).to be_present }
-      expect(Tag.count).to eq 4
+      expect do
+        [tag_child1, tag_child2, tag_grandchild, tag_great_grandchild].each { |t| expect(t).to be_present }
+      end.to change(Tag, :count).by 4 # We just want these tags
       expect(tag_child1.parent_tags).to eq([tag_parent])
       expect(tag_parent.child_tags.pluck(:id)).to match_array([tag_child1.id, tag_child2.id, tag_grandchild.id])
       expect(tag_grandchild.parent_tags.pluck(:id)).to match_array([tag_parent.id, tag_child2.id])
